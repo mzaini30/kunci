bf7e230a3cea74a211ba5bb8aa9fe74f4f984192
Zen <muhzaini30@gmail.com>
2022-07-09 10:56:43 +0800

    otomatis hapus file yang nggak penting

--- src/buka.ts
+++ src/buka.ts
@@ -2,6 +2,7 @@
 
 import { decrypt } from "crypto-zen";
 import { writeFileSync, readFileSync, existsSync } from "fs";
+import { $ } from "zx";
 
 const { parse } = JSON;
 
@@ -9,10 +10,14 @@ let kunci = readFileSync("kunci.txt").toString().trim();
 let listFile: string | string[] = readFileSync("kunci.json").toString();
 listFile = parse(listFile);
 
-for (let x of listFile) {
-  if (existsSync(`${x}.rahasia`)) {
-    let isinya = readFileSync(`${x}.rahasia`).toString("base64");
-    isinya = decrypt(isinya, kunci);
-    writeFileSync(x, isinya, "base64");
+async function init(): Promise<void> {
+  for (let x of listFile) {
+    if (existsSync(`${x}.rahasia`)) {
+      let isinya = readFileSync(`${x}.rahasia`).toString("base64");
+      isinya = decrypt(isinya, kunci);
+      writeFileSync(x, isinya, "base64");
+      await $`rm ${x}.rahasia`;
+    }
   }
 }
+init();
--- src/kunci.ts
+++ src/kunci.ts
@@ -3,6 +3,8 @@
 import { encrypt } from "crypto-zen";
 import { readFileSync, writeFileSync, existsSync, readFile } from "fs";
 import { argv } from "process";
+import { $ } from "zx";
+
 const { parse, stringify } = JSON;
 
 if (!existsSync(".gitignore")) {
@@ -20,35 +22,39 @@ function masukkanKeGitignore(file) {
 
 masukkanKeGitignore("kunci.txt");
 
-if (argv.length > 2) {
-  let listFile: string[] = [];
-  if (existsSync("kunci.json")) {
-    listFile = parse(readFileSync("kunci.json").toString());
-  }
-  if (!existsSync("kunci.txt")) {
-    console.log("Nggak ada file kunci.txt. Siapkan dulu ya...");
-  } else {
-    let filenya = argv.slice(2);
-    for (let x of filenya) {
-      masukkanKeGitignore(x);
-      if (!listFile.includes(x)) {
-        listFile.push(x);
-      }
+async function init(): Promise<void> {
+  if (argv.length > 2) {
+    let listFile: string[] = [];
+    if (existsSync("kunci.json")) {
+      listFile = parse(readFileSync("kunci.json").toString());
     }
-    writeFileSync("kunci.json", stringify(listFile, null, 2));
-  }
-} else {
-  let lihatYangMauDikunci: string | string[] =
-    readFileSync("kunci.json").toString();
-  lihatYangMauDikunci = parse(lihatYangMauDikunci);
-  for (let x of lihatYangMauDikunci) {
-    if (existsSync(x)) {
-      let kuncinya = readFileSync("kunci.txt").toString().trim();
-      let isinya = readFileSync(x).toString("base64");
-      let terkunci = encrypt(isinya, kuncinya);
-      writeFileSync(`${x}.rahasia`, terkunci, "base64");
+    if (!existsSync("kunci.txt")) {
+      console.log("Nggak ada file kunci.txt. Siapkan dulu ya...");
+    } else {
+      let filenya = argv.slice(2);
+      for (let x of filenya) {
+        masukkanKeGitignore(x);
+        if (!listFile.includes(x)) {
+          listFile.push(x);
+        }
+      }
+      writeFileSync("kunci.json", stringify(listFile, null, 2));
+    }
+  } else {
+    let lihatYangMauDikunci: string | string[] =
+      readFileSync("kunci.json").toString();
+    lihatYangMauDikunci = parse(lihatYangMauDikunci);
+    for (let x of lihatYangMauDikunci) {
+      if (existsSync(x)) {
+        let kuncinya = readFileSync("kunci.txt").toString().trim();
+        let isinya = readFileSync(x).toString("base64");
+        let terkunci = encrypt(isinya, kuncinya);
+        writeFileSync(`${x}.rahasia`, terkunci, "base64");
+        await $`rm ${x}`;
+      }
     }
   }
 }
+init();
 
 writeFileSync(".gitignore", gitignore.join("\n"));
